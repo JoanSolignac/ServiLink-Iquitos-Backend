4c385a2744bed18f058b59ce13973e3ea36d0299
Joan Piero Solignac Lovera <joanpsolignac@gmail.com>
2026-06-22 07:35:25 -0500

    refactor project

--- src/modules/profiles/features/find-profile-by-user-id.feature.ts
+++ src/modules/profiles/features/find-profile-by-user-id.feature.ts
@@ -7,7 +7,15 @@ import { ensureProfileNotExistsByUserId } from '@modules/profiles/utils/profile.
 export class FindProfileByUserIdFeature {
   constructor(private readonly prisma: PrismaService) {}
 
-  async execute(userId: string): Promise<Profile> {
-    return ensureProfileNotExistsByUserId(this.prisma, userId);
+  async execute(userId: string): Promise<Profile & { overallRating: number }> {
+    const [profile, ratingAgg] = await Promise.all([
+      ensureProfileNotExistsByUserId(this.prisma, userId),
+      this.prisma.rating.aggregate({
+        where: { service: { userId } },
+        _avg: { score: true },
+      }),
+    ]);
+
+    return { ...profile, overallRating: ratingAgg._avg.score ?? 0 };
   }
 }
